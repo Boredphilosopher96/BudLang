b0596c91758ed61e608b431a3df889dc8220aa33
SumukhNitundila <sumukh14@gmail.com>
2024-02-22 22:12:57 -0600

    Added one character token parsers

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,15 @@
 package lexer
 
+import (
+	"BudLang/token"
+	"go/token"
+
+	"github.com/sqls-server/sqls/token"
+)
+
+// TODO support unicode characters in variable names Can do this by changing Lexer.c from byte to rune
+// Unicode characters can be multiple bytes long
+
 type Lexer struct {
 	input        string
 	position     int
@@ -21,4 +31,47 @@ func (l *Lexer) readChar() {
 	}
 	l.readPosition = l.position
 	l.readPosition += 1
+}
+
+func (l *Lexer) NextToken() *token.Token {
+	var t token.Token
+
+	switch l.c {
+	case "=":
+		t = token.NewToken(token.ASSIGN, l.c)
+
+	case "(":
+		t = token.NewToken(token.LPAREN, l.c)
+
+	case ")":
+		t = token.NewToken(token.RPAREN, l.c)
+
+	case "{":
+		t = token.NewToken(token.LBRACE, l.c)
+
+	case "}":
+		t = token.NewToken(token.RBRACE, l.c)
+
+	case "+":
+		t = token.NewToken(token.ADD, l.c)
+
+	case "/":
+		t = token.NewToken(token.DIVIDE, l.c)
+
+	case "*":
+		t = token.NewToken(token.MULTIPLY, l.c)
+
+	case ";":
+		t = token.NewToken(token.SEMICOLON, l.c)
+
+	case ",":
+		t = token.NewToken(token.COMMA, l.c)
+
+	case 0:
+		t.Literal = ""
+		t.Type = token.EOF
+	}
+
+	l.readChar()
+	return t
 }
--- token/token.go
+++ token/token.go
@@ -3,33 +3,34 @@ package token
 type TokenType string
 
 type Token struct {
-    Type TokenType
-    Literal string
+	Type    TokenType
+	Literal string
 }
 
-
 const (
-    ILLEGAL = "ILLEGAL"
-    EOF = "EOF"
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
 
+	IDENT = "IDENT"
+	INT   = "INT"
 
-    IDENT = "IDENT"
-    INT = "INT"
+	ASSIGN   = "="
+	PLUS     = "+"
+	DIVIDE   = "/"
+	MULTIPLY = "*"
 
+	COMMA     = ","
+	SEMICOLON = ";"
 
-    ASSIGN = "="
-    PLUS = "+"
-    MINUS = "-"
-    DIVIDE = "/"
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
 
-    COMMA = ","
-    SEMICOLON = ";"
-
-    LPAREN = "("
-    RPAREN = ")"
-    LBRACE = "{"
-    RBRACE = "}"
-
-    FUNCTION = "FUNCTION"
-    LET = "LET"
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
 )
+
+func NewToken(tokenType Token.TokenType, c byte) Token {
+	return Token{Token.Type: tokenType, Token.Literal: str(c)}
+}
