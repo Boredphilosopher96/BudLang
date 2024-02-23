0a9b3aace2498a96e750ed21146a70c52d9247af
SumukhNitundila <sumukh14@gmail.com>
2024-02-23 17:19:54 -0600

    Working lexer with a REPL like console

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"BudLang/repl"
+	"fmt"
+	"os"
+	"os/user"
+)
+
+func main() {
+    user, err := user.Current()
+    if err != nil {
+        panic(err)
+    }
+    fmt.Println("Hey %s!! You are in the budlang console!",user.Username)
+    fmt.Println("Type in commands")
+
+    repl.Start(os.Stdin, os.Stdout)
+
+}
---
+++ repl/repl.go
@@ -0,0 +1,31 @@
+package repl
+
+import (
+	"BudLang/lexer"
+	"BudLang/token"
+	"bufio"
+	"fmt"
+	"io"
+)
+
+const PROMPT = ">>"
+
+func Start(in io.Reader, out io.Writer) {
+    scanner := bufio.NewScanner(in)
+
+    for {
+        fmt.Fprintf(out, PROMPT)
+        scanned := scanner.Scan()
+        if !scanned {
+            return
+        }
+
+        line := scanner.Text()
+        l := lexer.NewLexer(line)
+
+
+        for t := l.NextToken(); t.Type != token.EOF; t = l.NextToken() {
+            fmt.Fprintf(out, "%v\n", t)
+        }
+    }
+}
